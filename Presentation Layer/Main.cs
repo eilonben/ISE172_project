61bb32e2c83b5fe4de9ea8f6f2b446d1eac0eee3
shaioz <shaioz2005@gmail.com>
2017-03-26 20:21:02 +0300

    idkun

--- Presentation Layer/Main.cs
+++ Presentation Layer/Main.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Data Access Layer;
+using DataAccessLayer;
 
 namespace Presentation_Layer
 {
@@ -12,28 +12,48 @@ namespace Presentation_Layer
         public static void main(String[] args)
         {
             Console.WriteLine("Hello, please enter your request type number: 1-Buy, 2-Sell, 3-Cancle, 4-Query.");
-            var type = Console.ReadLine();
-            if (type == "1")
+            int type = Convert.ToInt32(Console.ReadLine());
+            if (type == 1)
             {
-                Console.WriteLine("Please enter the commodity id, amount and ask price.");
+                Console.WriteLine("Please enter the Commodity ID.");
+                int commodityID = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the amount.");
+                int ammount = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the price.");
+                int price = Convert.ToInt32(Console.ReadLine());
             }
-            if (type == "2")
+            if (type == 2)
             {
-                Console.WriteLine("Please enter the commodity id, amount and ask price.");
+                Console.WriteLine("Please enter the Commodity ID.");
+                int commodityID = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the amount.");
+                int ammount = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the price.");
+                int price = Convert.ToInt32(Console.ReadLine());
             }
-            if (type == "3")
+            if (type == 3)
             {
                 Console.WriteLine();
             }
-            if (type == "4")
+            if (type == 4)
             {
                 Console.WriteLine("What would you like? 1-Query sell/buy, 2-Query user, 3-Query market.");
-                var queryChoise = Console.ReadLine();
+                int queryType = Convert.ToInt32(Console.ReadLine());
+                if (queryType == 1)
+                {
+                    Console.WriteLine("Please enter the Buy or the Sell Rrequest ID.");
+                    int requestID = Convert.ToInt32(Console.ReadLine());
+                }
+                if (queryType == 2)
+                {
 
-
+                }
+                if (queryType == 3)
+                {
+                    Console.WriteLine("Please enter the Commodity ID.");
+                    int commodityID = Convert.ToInt32(Console.ReadLine());
+                }
             }
-
-
         }
     }
 }
