907dff7952962db1c9940bdc00c1e18637f2022f
eilonben <eilonben@post.bgu.ac.il>
2017-03-26 19:59:15 +0300

    DAL Changes

---
+++ DataAccessLayer/BuyRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class BuyRequest
+    {
+        public const string type = "buy";
+        public int commodity;
+        public int amount;
+        public int price;
+    }
+}
---
+++ DataAccessLayer/SellRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class SellRequest
+    {
+        public const string type = "sell";
+        public int commodity;
+        public int amount;
+        public int price;
+    }
+}
