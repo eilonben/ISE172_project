8e2c213829ddafb09274ef27501b7f5b7e45e2ff
eilonben <eilonben@post.bgu.ac.il>
2017-05-11 16:22:15 +0300

    Starting the GUI

---
+++ Presentation Layer 2/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentation_Layer_2
+{
+    static class Main1
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UserInterface());
+        }
+    }
+}
