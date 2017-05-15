b9fa32ba655aadc1961e72da1d189cdd48fea9ca
shaioz <shaioz2005@gmail.com>
2017-05-15 17:32:46 +0300

    fixed logFile

--- Business Layer/RequestAgent.cs
+++ Business Layer/RequestAgent.cs
@@ -6,25 +6,38 @@ using System.Threading.Tasks;
 using DataAccessLayer;
 using MarketClient;
 using MarketClient.DataEntries;
+using log4net;
 
 namespace Business_Layer
 {
     public class RequestAgent
     {
         RequestManager rm = new RequestManager();
+        ILog myLogger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        
 
         public string buyCommodities(int price, int commodity, int amount) {
             int response = rm.SendBuyRequest(price, commodity, amount);
             if (response == -1)
+            {
+                myLogger.Error("You asked to buy "+amount+" commodities of type "+commodity+" in the price of "+price+ ". An error has occured and your Request was incomplete. more info:" + rm.error);
                 return ("An error has occured and your Request was incomplete. more info:" + rm.error);
+            }
+            myLogger.Error("You asked to buy " + amount + " commodities of type " + commodity + " in the price of " + price + ". Your Request has been successful. Request ID:" + response);
             return ("Your Request has been successful. Request ID:" + response);
+            
         }
 
         public string sellCommodities(int price, int commodity, int amount)
         {
             int response = rm.SendSellRequest(price, commodity, amount);
             if (response == -1)
+            {
+                myLogger.Error("You asked to sell "+amount+" commodities of type "+commodity+" in the price of "+price+". An error has occured and your Request was incomplete.more info: " + rm.error);
                 return ("An error has occured and your Request was incomplete. more info:" + rm.error);
+            }
+            myLogger.Info("You asked to sell "+amount+" commodities of type "+commodity+" in the price of "+price+". Your Request has been successful. Request ID:" + response);
             return ("Your Request has been successful. Request ID:" + response);
         }
 
@@ -32,7 +45,11 @@ namespace Business_Layer
         {
             bool response = rm.SendCancelBuySellRequest(id);
             if (response)
+            {
+                myLogger.Info("You asekd to cancel the Request "+id+ ". Request was canceled successfuly");
                 return ("Request was canceled successfuly");
+            }
+            myLogger.Error("You asked to cancel the Request " + id + ". An Error has occured. more info:" + rm.error);    
             return ("An Error has occured. more info:" + rm.error);       
         }
 
@@ -40,15 +57,24 @@ namespace Business_Layer
         {
             var response = rm.SendQueryBuySellRequest(id);
             if (response == null)
+            {
+                myLogger.Error("You requested Query Buy/Sell " + id + ". An error has occured. more info:" + rm.error);
                 return ("An error has occured. more info:" + rm.error);
+            }
+            myLogger.Info("You requested Query Buy/Sell "+id+". " + response.ToString());
             return response.ToString();
+            
         }
 
         public string CommodityQuery(int id)
         {
             var response = rm.SendQueryMarketRequest(id);
             if (response == null)
+            {
+                myLogger.Error("You requested Commodity Query "+id+ ". An error has occured. more info:" + rm.error);
                 return ("An error has occured. more info:" + rm.error);
+            }
+            myLogger.Info("You requested Commodity Query " + id + ". "+response.ToString());    
             return response.ToString();
         }
 
@@ -56,37 +82,29 @@ namespace Business_Layer
         {
             var response = rm.SendQueryUserRequest();
             if (response == null)
+            {
+                myLogger.Error("You requested User Query. An error has occured. More info: "+rm.error);
                 return ("An error has occured. more info:" + rm.error);
+            }
+            myLogger.Info("You requested User Query. "+response.ToString());   
             return response.ToString();
         }
         public string AllMarketQuery()
         {
             var response = rm.SendAllMarketQuery();
             if (response == null)
-                return ("An error has occured. more info:" + rm.error);
-            string output = "Status of all commodities: \n";
-            foreach (AllCommodityOffer e in response)
             {
-                output += e.ToString();
-                output += "\n";
-            }
-            return output;
+                myLogger.Error("You requested user All Market Query. An error has occured. more info:" + rm.error);
+                return ("An error has occured. more info:" + rm.error);
         }
 
         public string UserRequestsQuery()
         {
             List<MarketUserRequests> response = rm.SendUserRequestsQuery();
             if (response == null)
+            {
+                myLogger.Error("You requested User Request Query. An error has occured. more info:" + rm.error);
                 return ("An error has occured. more info:" + rm.error);
-            string output = "";
-            if (response.Count == 0)
-                return "You have no active requests.";
-            foreach (MarketUserRequests e in response) {
-                output += "All The requests of user32: \n";
-                output += e.ToString();
-                output += "\n";
-            }
-            return output;
         }
     }
 }
